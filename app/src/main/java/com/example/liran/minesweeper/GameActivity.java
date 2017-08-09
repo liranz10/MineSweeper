c9e1cbf849fd4fad5a76d4621e770bff94cc6b7e
Liran <liranz10z@gmail.com>
2017-08-09 14:49:11 +0300

    1.2

--- app/src/main/java/com/example/liran/minesweeper/GameActivity.java
+++ app/src/main/java/com/example/liran/minesweeper/GameActivity.java
@@ -2,14 +2,12 @@ package com.example.liran.minesweeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
     }
 }
