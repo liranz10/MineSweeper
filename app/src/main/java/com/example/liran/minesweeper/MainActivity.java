5e04f40bdf4ea7e9ede8af27637f6ff8c01efb60
Liran <liranz10z@gmail.com>
2017-08-11 10:50:57 +0300

    1.3

--- app/src/main/java/com/example/liran/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/liran/minesweeper/MainActivity.java
@@ -1,15 +1,44 @@
 package com.example.liran.minesweeper;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_main);
+
+        final Intent selectlevel = new Intent(this,SelectLevelActivity.class);
+        final Intent highscore = new Intent(this,HighScoreActivity.class);
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(selectlevel);
+            }
+        });
+        findViewById(R.id.high_score).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(highscore);
+            }
+        });
+
+        findViewById(R.id.exit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+
 
     }
 }
